a2d3471251d26fc76bb5bff97d399194563868fa
FranciscaLiliane <42254398+FranciscaLiliane@users.noreply.github.com>
2022-02-18 09:56:06 -0300

    Add files via upload

---
+++ Exercício 13/Entites/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercício_13.Entites
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder)
+        {
+            Number = number;
+            Holder = holder;
+        }
+
+        public Account(int number, string holder, double balance) : this(number, holder)
+        {
+            Balance = balance;
+        }
+        public virtual void Whthdraw(double amount)
+        {
+            Balance -= amount + 5;
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ Exercício 13/Program.cs
@@ -0,0 +1,20 @@
+﻿using Exercício_13.Entites;
+using System;
+
+namespace Exercício_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+            Account acc1 = new Account(1001, "ALex", 500.0);
+            Account acc2 = new SavingsAccount(1002, "Anna", 500.0, 0.01);
+
+            acc1.Whthdraw(10.0);
+            acc2.Whthdraw(10.0);
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
+        }
+    }
+}
